5b198cf7ab93a3a91072a5110c8125ae3cd3eab9
Mohd Muneer Arif <muneer.arif@paytm.com>
2017-09-27 15:12:04 +0530

    Health Controller and common Module with health response

--- controllers/src/main/java/com/asce/oauth/Application.java
+++ controllers/src/main/java/com/asce/oauth/Application.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.asce.oauth.controllers;
+package com.asce.oauth;
 
 
 import java.util.logging.Logger;
